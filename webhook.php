d28611fb07797c2d71e313ca75fe8e57115137d2
Matias Ziliotto <matiziliotto1@gmail.com>
2020-08-21 02:22:59 -0300

    WebHook x1001221123

--- webhook.php
+++ webhook.php
@@ -1,10 +1,4 @@
 <?php
-    http_response_code(200);
-
-    require __DIR__ .  '/vendor/autoload.php';
-
-    MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
-
     $myfile = fopen("info.txt", "w") or die("Unable to open file!");
     $txt = json_encode($_REQUEST);
     fwrite($myfile, $txt);
@@ -19,6 +13,12 @@
     $txt = json_encode($_GET);
     fwrite($myfile, $txt);
     fclose($myfile);
+
+    http_response_code(200);
+
+    require __DIR__ .  '/vendor/autoload.php';
+
+    MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 
     switch($_POST["type"]) {
         case "payment":
