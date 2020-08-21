c8617bd34255af1a9d9f4b876278263cd9a973b9
Matias Ziliotto <matiziliotto1@gmail.com>
2020-08-20 22:03:12 -0300

    Pruebas webhooks x11

--- readTxt.php
+++ readTxt.php
@@ -1,5 +1,5 @@
 <?php
-    echo "holaaa probando 1";
+    echo "holaaa probando 2";
     $myfile = fopen("info.txt", "r") or die("Unable to open file!");
     echo fread($myfile,filesize("info.txt"));
     fclose($myfile);
