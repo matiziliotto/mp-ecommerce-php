f0baff1e49ab5395ee4f196185c7ad1e93567f11
Matias Ziliotto <matiziliotto1@gmail.com>
2020-08-21 02:08:18 -0300

    WebHook x10012211

--- detail.php
+++ detail.php
@@ -9,6 +9,7 @@
         access-token: APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398
     */
     MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
+    MercadoPago\SDK::setPublicKey("APP_USR-7eb0138a-189f-4bec-87d1-c0504ead5626");
     MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
     // Crea un objeto de preferencia
